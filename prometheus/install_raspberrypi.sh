37a272e5449a8fc3b3b06f1e8c002b12e56b2f7b
Mihai Galos <mihaigalos@gmail.com>
2020-04-04 13:07:08 +0200

    Split into function

--- prometheus/install_raspberrypi.sh
+++ prometheus/install_raspberrypi.sh
@@ -5,26 +5,31 @@ ARCH=${ARCH:-arm64}
 VERSION=${VERSION:-2.17.1}
 TARGET=prometheus-${VERSION}.linux-${ARCH}
 
-sudo useradd --no-create-home --shell /bin/false prometheus
+install_binary(){
+    sudo useradd --no-create-home --shell /bin/false prometheus
 
-sudo mkdir /etc/prometheus
-sudo chown prometheus:prometheus /etc/prometheus
+    sudo mkdir /etc/prometheus
+    sudo chown prometheus:prometheus /etc/prometheus
 
-sudo mkdir /var/lib/prometheus
-sudo chown prometheus:prometheus /var/lib/prometheus
+    sudo mkdir /var/lib/prometheus
+    sudo chown prometheus:prometheus /var/lib/prometheus
 
-pushd /tmp
-curl -LO https://github.com/prometheus/prometheus/releases/download/v${VERSION}/${TARGET}.tar.gz
-tar -xvf ${TARGET}.tar.gz
+    pushd /tmp
+    curl -LO https://github.com/prometheus/prometheus/releases/download/v${VERSION}/${TARGET}.tar.gz
+    tar -xvf ${TARGET}.tar.gz
 
-sudo cp ${TARGET}/prometheus /usr/local/bin/
-sudo chown prometheus:prometheus /usr/local/bin/prometheus
+    sudo cp ${TARGET}/prometheus /usr/local/bin/
+    sudo chown prometheus:prometheus /usr/local/bin/prometheus
 
-sudo cp -r ${TARGET}/consoles /etc/prometheus
-sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-sudo cp -r ${TARGET}/console_libraries /etc/prometheus
-sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+    sudo cp -r ${TARGET}/consoles /etc/prometheus
+    sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+    sudo cp -r ${TARGET}/console_libraries /etc/prometheus
+    sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 
-rm -rf ${TARGET}.tar.gz ${TARGET}
+    rm -rf ${TARGET}.tar.gz ${TARGET}
 
-popd
+    popd
+}
+
+install_binary
+
