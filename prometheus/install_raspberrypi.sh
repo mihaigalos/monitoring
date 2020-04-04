38577216fc8b8ea61171dbd039d7e090d97573b7
Mihai Galos <mihaigalos@gmail.com>
2020-04-04 11:02:01 +0200

    Add missing /var/lib folders for prometheus

--- prometheus/install_raspberrypi.sh
+++ prometheus/install_raspberrypi.sh
@@ -9,6 +9,9 @@ sudo useradd --no-create-home --shell /bin/false prometheus
 sudo mkdir /etc/prometheus
 sudo chown prometheus:prometheus /etc/prometheus
 
+sudo mkdir /var/lib/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
+
 pushd /tmp
 curl -LO https://github.com/prometheus/prometheus/releases/download/v${VERSION}/prometheus-${VERSION}.linux-${ARCH}.tar.gz
 tar -xvf prometheus-${VERSION}.linux-${ARCH}.tar.gz
@@ -21,4 +24,4 @@ sudo chown -R prometheus:prometheus /etc/prometheus/consoles
 
 rm -rf prometheus-${VERSION}.linux-${ARCH}.tar.gz prometheus-${VERSION}.linux-${ARCH}
 
-
+popd
