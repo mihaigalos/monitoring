33ce593b967a10aa817e0b7fdee042300d09afc2
Mihai Galos <mihaigalos@gmail.com>
2020-04-04 13:09:37 +0200

    Use install_service function

--- prometheus/install_raspberrypi.sh
+++ prometheus/install_raspberrypi.sh
@@ -31,5 +31,28 @@ install_binary(){
     popd
 }
 
+install service() {
+    cat <<EOF > /etc/systemd/system/prometheus.service
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
 install_binary
+install_service
 
