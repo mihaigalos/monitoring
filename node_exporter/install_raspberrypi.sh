cb3feaf224d66884a961a3bb787a8e24e9916883
Mihai Galos <mihaigalos@gmail.com>
2020-04-04 13:11:00 +0200

    Adjust identation

--- node_exporter/install_raspberrypi.sh
+++ node_exporter/install_raspberrypi.sh
@@ -23,20 +23,19 @@ install_binary(){
 
 install_service() {
     cat <<EOF >/etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
 
-    [Unit]
-    Description=Node Exporter
-    Wants=network-online.target
-    After=network-online.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
 
-    [Service]
-    User=node_exporter
-    Group=node_exporter
-    Type=simple
-    ExecStart=/usr/local/bin/node_exporter
-
-    [Install]
-    WantedBy=multi-user.target
+[Install]
+WantedBy=multi-user.target
 
 EOF
 
