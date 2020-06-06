1791c93be947fbbaa7301a9b2b38a7048a8be201
Mihai Galos <mihaigalos@gmail.com>
2020-06-06 19:35:47 +0000

    Use ramdisk for data to minimize microSD wear

---
+++ prometheus/use_ramdisk.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# set up ramdisk
+sudo mkdir -p /mnt/prometheus_logs
+sudo chown $USER:$USER /mnt/prometheus_logs
+sudo mount -t tmpfs -o size=100M tmpfs /mnt/prometheus_logs
+
+# copy logs to ramdisk
+sudo systemctl stop prometheus
+cp -r data /mnt/prometheus_logs
+rm -r data
+ln -s /mnt/prometheus_logs/data .
+
+sudo systemctl restart prometheus
+
+# sync to disk every night at 04:00
+(crontab -l ; echo "0 4 * * * mkdir -p $HOME/prometheus_logs && cp -r /mnt/prometheus_logs/data  $HOME/prometheus_logs") | crontab -
+
+# make it permanent after reboot
+grep /mnt/prometheus_logs /etc/mtab | sudo tee -a /etc/fstab
